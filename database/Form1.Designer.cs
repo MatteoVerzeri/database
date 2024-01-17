77875a2a4892fa893c64b775b82e47266a9a8a75
Verzeri.19332 <Verzeri.19332@LABI1-6PC15>
2024-01-17 11:50:52 +0100

    Aggiungere i file di progetto.

---
+++ database/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(203, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(536, 337);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.comboBox3);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Proprietari";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Resetta filtri";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.comboBox5);
+            this.tabPage2.Controls.Add(this.comboBox4);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.comboBox2);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Automobili";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Resetta filtri";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(34, 31);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(203, 31);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(536, 337);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(768, 400);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Riparazioni";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(92, 29);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(536, 337);
+            this.dataGridView3.TabIndex = 1;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.dataGridView4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(768, 400);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Tagliandi";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(116, 32);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(536, 337);
+            this.dataGridView4.TabIndex = 1;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.dataGridView5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(768, 400);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Tagliandi eseguiti";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(116, 32);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(536, 337);
+            this.dataGridView5.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(24, 82);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(34, 75);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 4;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(34, 117);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 5;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox4;
+    }
+}
+
---
+++ database/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=roott;pwd=roott;database=macchina";
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+            conn.Open();
+            String sql1 = "select nome, cognome, auto, id from proprietario;";
+            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+            cmd1.ExecuteNonQuery();
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = cmd1;
+            DataTable dati = new DataTable();
+            MyAdapter.Fill(dati);
+
+            
+            //List<string> list = new List<string>();
+            //for (int i = 0; i<comboBox1.MaxLength; i++)
+            //{
+            //    list[i] = comboBox1.Items[i].ToString();
+            //}
+            //list.Distinct();
+            //for(int i = 0; i<list.Count; i++)
+            //{
+            //    comboBox1.Items.Add(list[i]);
+            //}
+            comboBox1.DisplayMember = "nome";
+            comboBox1.ValueMember = "id";
+            comboBox1.DataSource = dati.DefaultView.ToTable(true, "nome").Copy();
+
+            comboBox3.DisplayMember = "auto";
+            comboBox3.ValueMember = "id";
+            comboBox3.DataSource = dati.DefaultView.ToTable(true, "auto").Copy();
+
+            String sql2 = "select automobile.marca, automobile.modello, automobile.prezzo, automobile.riparazioni, automobile.id from automobile;";
+            cmd1 = new MySqlCommand(sql2, conn);
+            cmd1.ExecuteNonQuery();
+            MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = cmd1;
+            DataTable dati2 = new DataTable();
+            MyAdapter.Fill(dati2);
+
+            comboBox2.DisplayMember = "marca";
+            comboBox2.ValueMember = "id";
+            comboBox2.DataSource = dati2.DefaultView.ToTable(true, "marca").Copy();
+
+            comboBox4.DisplayMember = "modello";
+            comboBox4.ValueMember = "id";
+            comboBox4.DataSource = dati2.DefaultView.ToTable(true, "modello").Copy();
+
+            comboBox5.DisplayMember = "prezzo";
+            comboBox5.ValueMember = "id";
+            comboBox5.DataSource = dati2.DefaultView.ToTable(true, "prezzo").Copy();
+
+            String sql3 = "select riparazione.descrizione from riparazione;";
+            cmd1 = new MySqlCommand(sql3, conn);
+            cmd1.ExecuteNonQuery();
+            MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = cmd1;
+            DataTable dati3 = new DataTable();
+            MyAdapter.Fill(dati3);
+
+
+            String sql4 = "select tagliando.descrizione from tagliando;";
+            cmd1 = new MySqlCommand(sql4, conn);
+            cmd1.ExecuteNonQuery();
+            MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = cmd1;
+            DataTable dati4 = new DataTable();
+            MyAdapter.Fill(dati4);
+
+
+            String sql5 = "select esegue.costo, esegue.data, esegue.id_macchina, esegue.id_tagliando from esegue;";
+            cmd1 = new MySqlCommand(sql5, conn);
+            cmd1.ExecuteNonQuery();
+            MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = cmd1;
+            DataTable dati5 = new DataTable();
+            MyAdapter.Fill(dati5);
+
+
+            dataGridView1.DataSource = dati;
+            dataGridView1.Columns.RemoveAt(3);
+            dataGridView2.DataSource = dati2;
+            dataGridView2.Columns.RemoveAt(4);
+            dataGridView3.DataSource = dati3;
+            dataGridView4.DataSource = dati4;
+            dataGridView5.DataSource = dati5;
+            conn.Close();
+
+        }
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=roott;pwd=roott;database=macchina";
+
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                String sql1 = "SELECT nome, cognome, auto FROM proprietario WHERE nome = @nome";
+
+                using (MySqlCommand cmd1 = new MySqlCommand(sql1, conn))
+                {
+                    cmd1.Parameters.AddWithValue("@nome", comboBox1.Text);
+
+                    using (MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1))
+                    {
+                        DataTable dati = new DataTable();
+                        MyAdapter.Fill(dati);
+                        dataGridView1.DataSource = dati;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=roott;pwd=roott;database=macchina";
+
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                String sql1 = "SELECT nome, cognome, auto FROM proprietario";
+
+                using (MySqlCommand cmd1 = new MySqlCommand(sql1, conn))
+                {
+                    cmd1.Parameters.AddWithValue("@nome", comboBox1.Text);
+
+                    using (MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1))
+                    {
+                        DataTable dati = new DataTable();
+                        MyAdapter.Fill(dati);
+                        dataGridView1.DataSource = dati;
+                    }
+                }
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=roott;pwd=roott;database=macchina";
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                String sql2 = "SELECT marca, modello, prezzo, riparazioni FROM automobile WHERE marca = @marca";
+
+                using (MySqlCommand cmd1 = new MySqlCommand(sql2, conn))
+                {
+                    cmd1.Parameters.AddWithValue("@marca", comboBox2.Text);
+
+                    using (MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1))
+                    {
+                        DataTable dati2 = new DataTable();
+                        MyAdapter.Fill(dati2);
+                        dataGridView2.DataSource = dati2;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=roott;pwd=roott;database=macchina";
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                String sql2 = "SELECT marca, modello, prezzo, riparazioni FROM automobile";
+
+                using (MySqlCommand cmd1 = new MySqlCommand(sql2, conn))
+                {
+                    cmd1.Parameters.AddWithValue("@marca", comboBox2.Text);
+
+                    using (MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1))
+                    {
+                        DataTable dati2 = new DataTable();
+                        MyAdapter.Fill(dati2);
+                        dataGridView2.DataSource = dati2;
+                    }
+                }
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=roott;pwd=roott;database=macchina";
+
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                String sql1 = "SELECT nome, cognome, auto FROM proprietario WHERE auto = @auto";
+
+                using (MySqlCommand cmd1 = new MySqlCommand(sql1, conn))
+                {
+                    cmd1.Parameters.AddWithValue("@auto", comboBox3.Text);
+
+                    using (MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1))
+                    {
+                        DataTable dati = new DataTable();
+                        MyAdapter.Fill(dati);
+                        dataGridView1.DataSource = dati;
+                    }
+                }
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=roott;pwd=roott;database=macchina";
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                String sql2 = "SELECT marca, modello, prezzo, riparazioni FROM automobile WHERE modello = @modello";
+
+                using (MySqlCommand cmd1 = new MySqlCommand(sql2, conn))
+                {
+                    cmd1.Parameters.AddWithValue("@modello", comboBox4.Text);
+
+                    using (MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1))
+                    {
+                        DataTable dati2 = new DataTable();
+                        MyAdapter.Fill(dati2);
+                        dataGridView2.DataSource = dati2;
+                    }
+                }
+            }
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=roott;pwd=roott;database=macchina";
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                String sql2 = "SELECT marca, modello, prezzo, riparazioni FROM automobile WHERE prezzo = @prezzo";
+
+                using (MySqlCommand cmd1 = new MySqlCommand(sql2, conn))
+                {
+                    cmd1.Parameters.AddWithValue("@prezzo", comboBox5.Text);
+
+                    using (MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1))
+                    {
+                        DataTable dati2 = new DataTable();
+                        MyAdapter.Fill(dati2);
+                        dataGridView2.DataSource = dati2;
+                    }
+                }
+            }
+        }
+    }
+}
